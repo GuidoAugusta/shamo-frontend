78651cd47787029a8b973b63465bc6c83d727a58
GuidoAugusta <muhammadguido29@students.amikom.ac.id>
2024-02-21 22:18:10 +0700

    membuat header dan popular products pada homepage

--- lib/theme.dart
+++ lib/theme.dart
@@ -12,9 +12,11 @@ Color backgroundColor2 = Color(0xff2B2937);
 Color backgroundColor3 = Color(0xff242231);
 Color backgroundColor4 = Color(0xff252836);
 
-Color primaryTextColor = Color(0xffE1E1E1);
+Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
 Color subtitleColor = Color(0xff504F5E);
+Color transparentColor = Colors.transparent;
+Color blackColor = Color(0xff2E2E2E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -38,6 +40,10 @@ TextStyle alertTextStyle = GoogleFonts.poppins(
 
 TextStyle purpleTextStyle = GoogleFonts.poppins(
   color: primaryColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
 );
 
 FontWeight light = FontWeight.w300;
